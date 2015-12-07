83e560f803c71e1b41662e0e27281f0fb37f3f8a
heiditan <heidi.tan.14@ucl.ac.uk>
2015-12-07 11:24:12 +0000

    Added password resets & email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-c9-heiditansl.c9.io'
+  host = 'rails-tutorial-c9-heiditansl.c9users.io'
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
