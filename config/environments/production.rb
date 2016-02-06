bcb5c80ec4ce6c4fd70252a9061a82bcc426e222
bill <1529248989@qq.com>
2016-02-06 23:29:12 +0000

    changing URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'pure-caverns-8278.herokuapp.com'
+  host = 'projectwebsite.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
