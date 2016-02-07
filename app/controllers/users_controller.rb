90ce7f48519df4b00af11de2a5c2a85ee200b946
bill <1529248989@qq.com>
2016-02-07 22:44:17 +0000

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to @user
+      redirect_to @user_url
     else
       render 'edit'
     end
