8dd2ffdaf8b5bcccd2f06e5e92ed20fc150377d6
bill <1529248989@qq.com>
2016-02-14 19:59:49 +0000

    change back

--- app/models/gallery.rb
+++
@@ -1,3 +0,0 @@
-class Gallery < ActiveRecord::Base
-    has_many :pictures, :dependent => :destroy
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
- 
-  has_many :assets
-  accepts_nested_attributes_for :assets, :allow_destroy => true
-  
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/  
   
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
