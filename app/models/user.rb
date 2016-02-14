624bf2a0f8df0eb60edf2a772184a170669fb627
bill <1529248989@qq.com>
2016-02-14 18:32:34 +0000

    multiple image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/  
+ 
+  has_many :assets
+  accepts_nested_attributes_for :assets, :allow_destroy => true
+  
   
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
