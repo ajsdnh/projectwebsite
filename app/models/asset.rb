03c74017a487c5ed050ffc19d7e4727592f07503
bill <1529248989@qq.com>
2016-02-14 19:54:05 +0000

    create gallery model

--- app/models/asset.rb
+++
@@ -1,5 +0,0 @@
-class Asset < ActiveRecord::Base
-    belongs_to :user
-  has_attached_file :image, :styles => { :medium => "640x480>", 
-                                         :thumb => "100x100#"}
-end
