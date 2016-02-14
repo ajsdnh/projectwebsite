c7bf0ce635281ca9514d758d63d0af08df631f90
bill <1529248989@qq.com>
2016-02-14 22:14:46 +0000

    user input video address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
+  myField = params[:thing][:field] 
+  
   def index
     @users = User.paginate(page: params[:page])
   end
